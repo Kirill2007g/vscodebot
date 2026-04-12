0b290856977522b337bf915da2885d935a716bbe
Kirill2007g <  Kirill2007g>
2026-04-12 19:23:02 +0300

    first commit

---
+++ run.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+from handlers import router
+from dotenv import BOT_TOKEN
+
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')    
